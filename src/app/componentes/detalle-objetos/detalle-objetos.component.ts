4a6eba585a7d0cced9a68c715c999fb6bd2022a0
1DeathSeeker <irwin1409@gmail.com>
2022-11-21 10:42:13 -0600

    ok

--- src/app/componentes/detalle-objetos/detalle-objetos.component.ts
+++ src/app/componentes/detalle-objetos/detalle-objetos.component.ts
@@ -9,17 +9,20 @@ import { ConsultasService } from 'src/app/services/consultas.service';
   styleUrls: ['./detalle-objetos.component.scss'],
 })
 export class DetalleObjetosComponent implements OnInit {
-
+//declaración de consulta y modal
   constructor(private servicioConsulta:ConsultasService, private modalCtrl:ModalController) { }
+  //Recepción de id
   @Input()id: number;
+  //declaración de objeto para interfaz con detalles del objeto a mostrar
   objeto:DataObjeto1={};
-
+  //función para cierre de ventana modal
   regresar() {
     this.modalCtrl.dismiss();
     }
 
   ngOnInit() {
-
+    ///Recuperación de datos en DataObjeto1 mediante la funcion getObjeto enviando
+    //el id, posteriormentes se realiza asignación al objeto "objetos"
     this.servicioConsulta.getObjeto(this.id)
     .subscribe((resp:DataObjeto1)=>{
       console.log('Objeto',resp)
--- src/app/pages/objetos/objetos.page.ts
+++ src/app/pages/objetos/objetos.page.ts
@@ -11,21 +11,21 @@ import { DetalleObjetosComponent } from 'src/app/componentes/detalle-objetos/det
   styleUrls: ['./objetos.page.scss'],
 })
 export class ObjetosPage implements OnInit {
-
+//ObjetoCon ingormación completa de los objetos del juego
   objetos:DataObjeto[]=[];
-
+//Declaración para consulta y modal
   constructor(private servicioConsulta:ConsultasService, private modalCtrl:ModalController) { }
-
+//Funcion asyncrona que nos permite el envio de ID al detalle del objeto seleccionado
   async verDetalleObjeto(id:number){
     const modal= await this.modalCtrl.create({
       component:DetalleObjetosComponent,
       componentProps:{id}
     });
-    modal.present();
+    modal.present(); //Presentación de la ventana modal
   }
 
   ngOnInit() {
-
+//Recuperación de datos en gameDB para su asignación al objeto "objetos"
     this.servicioConsulta.getDatos()
     .subscribe((resp:gameDB)=>{
       console.log('Objetos',resp)
